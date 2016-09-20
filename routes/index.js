b25757ce1c838f933133d85f798914ad862a8111
Michael Hudson <huddo121@gmail.com>
2016-09-20 13:45:46 +1000

    Begin displaying container summaries.
    Container summaries will be a way to collect information that is spread over several calls to the Docker Remote API in to one object, to eventually be displayed to the user in an easily digestible format

--- routes/index.js
+++ routes/index.js
@@ -35,7 +35,25 @@ router.get('/', function (req, res, next) {
 
 
     Promise.all([imagePromise, containerPromise, volumesPromise]).then((results) => {
-        res.render('index', {title: 'Behold', images: results[0], containers: results[1], volumes: results[2]});
+
+        //Summarise and aggregate all the data we can find for each container
+        let containerSummaries = results[1].map(container => {
+            let containerSummary = {};
+            containerSummary.Name = container.Name;
+
+            let containerImage = results[0].filter(image => image.Id === container.Image)[0];
+            if(containerImage === undefined) {
+                containerSummary.ImageName = "Unknown: " + container.Image;
+            } else {
+                containerSummary.ImageName = containerImage.RepoTags[0];
+            }
+
+            containerSummary.Status = container.State.Status;
+
+            return containerSummary;
+        });
+
+        res.render('index', {title: 'Behold', images: results[0], containers: results[1], volumes: results[2], containerSummaries: containerSummaries});
     });
 });
 
